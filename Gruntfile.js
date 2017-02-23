e2fc1d5ea57036c06cfcc80fc5f406bb9ab2e02e
Justin J. Novack <jnovack@parxcasino.com>
2017-02-23 14:37:02 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+    grunt.option('color', true);
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        nodemon: {
+            dev: {
+                script: 'index.js',
+                options: {
+                    watch: ['index.js', 'modules/**/*.js'],
+                    ext: 'js',
+                    ignore: ['node_modules/**', 'modules/webserver/public/**'],
+                    env: {
+                        NODE_ENV: 'dev'
+                    },
+                    nodeArgs: ['--debug'],
+                    cwd: __dirname,
+                    callback: function (nodemon) {
+                        nodemon.on('log', function (event) {
+                            console.log(event.colour);
+                        });
+                    },
+                }
+            }
+        },
+        watch: {
+            files: ['*.js', '**/*.js', '*.json', '!node_modules/**', '!.git/**', '!modules/webserver/public/**', '!bower_components/**', '!test/**', 'nodemon.json'],
+            tasks: ['jshint']
+        },
+        jshint: {
+            all: ['**/*.js', '**/*.json', '*.json', '!node_modules/**', '!modules/webserver/public/**', '!bower_components/**'],
+            options: {
+                esversion: 6
+            }
+        },
+        concurrent: {
+            options: {
+                "logConcurrentOutput": true
+            },
+            dev: {
+                tasks: ['nodemon', 'watch']
+            },
+            debug: {
+                tasks: ['nodemon', 'watch']
+            }
+        },
+        simplemocha: {
+            options: {
+                globals: [], // Add whitelisted globals here
+                timeout: 3000,
+                ignoreLeaks: false,
+                ui: 'bdd',
+                reporter: 'spec'
+            },
+            all: { src: ['test/**/*.js'] }
+        }
+    });
+
+    // Load plugins/tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-simple-mocha');
+
+    // Grunt task(s).
+    grunt.registerTask('default', ['jshint', 'nodemon:dev']);
+    grunt.registerTask('lint', ['jshint']);
+    grunt.registerTask('test', ['simplemocha']);
+};
---
+++ index.js
@@ -0,0 +1,54 @@
+var myApp = { };
+myApp.root = require('path').dirname(require.main.filename);
+myApp.package = require(myApp.root+"/package.json");
+
+// Load global event emitter
+var events = require('events');
+myApp.emitter = new events.EventEmitter();
+
+require("./modules/utils")(myApp);
+
+// Initialize myApp
+myApp.utils.consoleOutput("line");
+myApp.utils.consoleOutput(myApp.package.name + " v"+myApp.package.version, "Center", true);
+myApp.utils.consoleOutput(myApp.package.author, "Center", true);
+if(myApp.package.description !== undefined){
+    myApp.utils.consoleOutput(myApp.package.description, "Center", true);
+}
+myApp.utils.consoleOutput("line");
+
+// Start the initialization process
+myApp.utils.consoleOutput("Initializing...");
+
+// Save environment into a structure
+myApp.config = process.env;
+
+if (myApp.config.DEBUG === undefined) {
+    myApp.utils.consoleOutput("DEBUG is OFF - To enable debugging, set the DEBUG environment variable.");
+}
+/***
+ *  Initialize direct database access
+ ***/
+// require("../database/sqlite3")(myApp);
+// myApp.database.initialize();
+
+
+/***
+ *  Initialize simple persistent storage
+ ***/
+// require("../storage/redis")(myApp);
+require("./modules/storage/memory")(myApp);
+myApp.storage.initialize();
+
+
+/***
+ *  Initialize the webserver
+ ***/
+require("./modules/webserver/")(myApp);
+myApp.webserver.initialize();
+
+
+/***
+ *  Initialize the socket.io namespaces
+ ***/
+require("./modules/sockets/template")(myApp);
